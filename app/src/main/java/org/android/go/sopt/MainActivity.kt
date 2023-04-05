81d75b4f2415aa14a6b712f74cf49a63a15b82f2
gaeun <geun5744@gmail.com>
2023-04-05 14:44:55 +0900

    [feat] 회원가입 버튼 클릭시 화면전환

--- app/src/main/java/org/android/go/sopt/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/MainActivity.kt
@@ -1,11 +1,39 @@
 package org.android.go.sopt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewbinding.ViewBinding
+import com.google.android.material.snackbar.Snackbar
+import org.android.go.sopt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener{
+            Snackbar.make(
+                binding.root,
+                "안녕하세요",
+                Snackbar.LENGTH_LONG
+            ).show()
+
+        }
+
+        binding.signupButton.setOnClickListener {
+            val Intent = Intent(this,SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+
+
     }
 }
