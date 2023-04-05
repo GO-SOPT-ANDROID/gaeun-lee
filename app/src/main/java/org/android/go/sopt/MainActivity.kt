16999ee4fd74e14bc2ed167a898fed818a2d0514
gaeun <geun5744@gmail.com>
2023-04-05 22:48:01 +0900

    [feature] 회원가입 및 로그인 기능 구현

--- app/src/main/java/org/android/go/sopt/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/MainActivity.kt
@@ -1,39 +1,71 @@
 package org.android.go.sopt
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.viewbinding.ViewBinding
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import org.android.go.sopt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private var id:String = ""
+    private var password:String = ""
+    private var name:String = ""
+    private var mbti:String = ""
+
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                id = result.data?.getStringExtra("id") ?: ""
+                password = result.data?.getStringExtra("password")?:""
+                name= result.data?.getStringExtra("name")?:""
+                mbti = result.data?.getStringExtra("mbti")?:""
+
+
+            }
+        }
+
+
+
         binding.loginButton.setOnClickListener{
-            Snackbar.make(
-                binding.root,
-                "안녕하세요",
-                Snackbar.LENGTH_LONG
-            ).show()
+
+            Log.d("MainActivity","아이디 : ${id}")
+            Log.d("MainActivity","아이디 : ${binding.id.text}")
+
+            if(id== binding.id.text.toString()){
+
+
+
+                val intent = Intent(this,IntroduceActivity::class.java)
+
+                intent.putExtra("name",name)
+                intent.putExtra("mbti",mbti)
+                startActivity(intent)
+            }
 
         }
 
         binding.signupButton.setOnClickListener {
             val intent = Intent(this,SignupActivity::class.java)
-            startActivity(intent)
+            resultLauncher.launch(intent)
         }
 
-
-
-
-
-
-
     }
+
+
+
+
+
 }
--- app/src/main/java/org/android/go/sopt/SignupActivity.kt
+++ app/src/main/java/org/android/go/sopt/SignupActivity.kt
@@ -1,11 +1,33 @@
 package org.android.go.sopt
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.android.go.sopt.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.singupCompleteButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("id",binding.id.text.toString())
+            intent.putExtra("password",binding.password.text.toString())
+            intent.putExtra("name",binding.name.text.toString())
+            intent.putExtra("mbti",binding.mbti.text.toString())
+            setResult(RESULT_OK,intent)
+            finish()
+
+
+        }
+
+
+
     }
+
+
 }
