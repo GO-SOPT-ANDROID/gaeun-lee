9c44293d933c011110ef616bc520548cc64df0e6
gaeun <geun5744@gmail.com>
2023-04-09 20:32:50 +0900

    [Fix] 파일 이름 camelcase로 수정

--- app/src/main/java/org/android/go/sopt/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/SignUpActivity.kt
@@ -1,16 +1,14 @@
 package org.android.go.sopt
 
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
-import android.view.inputmethod.InputMethodManager
 import com.google.android.material.snackbar.Snackbar
 import org.android.go.sopt.databinding.ActivitySignupBinding
 
-class SignupActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
