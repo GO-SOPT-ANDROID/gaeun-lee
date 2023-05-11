ebe856a1b780c16d64a8890256b936343a6806c5
gaeun <geun5744@gmail.com>
2023-05-11 20:50:22 +0900

    [Add] week1 과제 가져오기

---
+++ app/src/main/java/org/android/go/sopt/myPage/IntroduceActivity.kt
@@ -0,0 +1,24 @@
+package org.android.go.sopt.myPage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.android.go.sopt.databinding.ActivityIntroduceBinding
+
+class IntroduceActivity : AppCompatActivity() {
+    lateinit var binding: ActivityIntroduceBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroduceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        var name = intent.getStringExtra("name") ?: ""
+        var speciality = intent.getStringExtra("speciality") ?: ""
+
+        binding.name.text = "이름 : $name"
+        binding.specialty.text = "특기 : $speciality"
+
+
+    }
+}
---
+++ app/src/main/java/org/android/go/sopt/myPage/LoginActivity.kt
@@ -0,0 +1,100 @@
+package org.android.go.sopt.myPage
+
+import android.app.Activity
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import org.android.go.sopt.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    private lateinit var id: String
+    private lateinit var password: String
+    private lateinit var name: String
+    private lateinit var speciality: String
+
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (MySharedPreferences.getUserId(this).isNullOrBlank()
+            || MySharedPreferences.getUserPass(this).isNullOrBlank()
+        ) {
+            clickLogin()
+            clickSignup()
+        } else {
+            alreadyLogin()
+        }
+
+
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        this.currentFocus?.let { hideKeyboard(it) }
+        return super.dispatchTouchEvent(ev)
+    }
+
+
+    private fun clickLogin() {
+        binding.btnLogin.setOnClickListener {
+
+
+            if (id == binding.etId.text.toString() && password == binding.etPassword.text.toString()) {
+
+
+                val intent = Intent(this, IntroduceActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or FLAG_ACTIVITY_NEW_TASK)
+
+                MySharedPreferences.setUserId(this, id)
+                MySharedPreferences.setUserPass(this, password)
+                MySharedPreferences.setUserName(this, name)
+                MySharedPreferences.setUserSpec(this, speciality)
+
+                intent.putExtra("name", name)
+                intent.putExtra("speciality", speciality)
+                startActivity(intent)
+                finish()
+
+            }
+
+        }
+    }
+
+    private fun clickSignup() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    id = result.data?.getStringExtra("id") ?: ""
+                    password = result.data?.getStringExtra("password") ?: ""
+                    name = result.data?.getStringExtra("name") ?: ""
+                    speciality = result.data?.getStringExtra("speciality") ?: ""
+
+
+                }
+            }
+        binding.btnSignup.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+    }
+
+    private fun alreadyLogin() {
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or FLAG_ACTIVITY_NEW_TASK)
+        val intent = Intent(this, IntroduceActivity::class.java)
+        intent.putExtra("name", MySharedPreferences.getUserName(this))
+        intent.putExtra("speciality", MySharedPreferences.getUserSpec(this))
+        startActivity(intent)
+        finish()
+    }
+
+
+}
