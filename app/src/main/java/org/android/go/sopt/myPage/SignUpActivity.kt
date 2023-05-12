b6e4ed5f4ec55543e53e112025e863358276711f
gaeun <geun5744@gmail.com>
2023-05-12 14:57:16 +0900

    [Feature/#7] 회원가입 api 연동

--- app/src/main/java/org/android/go/sopt/myPage/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/myPage/SignUpActivity.kt
@@ -3,11 +3,24 @@ package org.android.go.sopt.myPage
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.view.MotionEvent
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.google.android.material.snackbar.Snackbar
+import org.android.go.sopt.RequestSignUpDto
+import org.android.go.sopt.ResponseSignUpDto
 import org.android.go.sopt.databinding.ActivitySignupBinding
+import org.android.go.sopt.remote.ServicePool
+import retrofit2.Call
+import retrofit2.Response
+import android.text.TextWatcher as TextWatcher
+
 
 class SignUpActivity : AppCompatActivity() {
+
+    private val signUpService = ServicePool.signUpService
+
     lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,24 +41,49 @@ class SignUpActivity : AppCompatActivity() {
         return super.dispatchTouchEvent(ev)
     }
 
-    private fun signUp(){
+    private fun signUp() {
         binding.btnSignup.setOnClickListener {
-            if (binding.etId.text.length in 6..10 && binding.etPassword.text.length in 8..12){
-                val intent = Intent(this, LoginActivity::class.java)
+            if (binding.etId.text.length in 6..10 && binding.etPassword.text.length in 8..12) {
 
-                intent.putExtra("id",binding.etId.text.toString())
-                intent.putExtra("password",binding.etPassword.text.toString())
-                intent.putExtra("name",binding.etName.text.toString())
-                intent.putExtra("speciality",binding.etSpeciality.text.toString())
-                setResult(RESULT_OK,intent)
-                finish()
-                Snackbar.make(
-                    binding.root,
-                    "회원가입이 완료되었다.",
-                    Snackbar.LENGTH_SHORT
-                ).show()
-            }
-            else{
+                signUpService.login(
+                    with(binding) {
+                        RequestSignUpDto(
+                            etId.text.toString(),
+                            etPassword.text.toString(),
+                            etName.text.toString(),
+                            etSpeciality.text.toString()
+
+                        )
+                    }
+                ).enqueue(object : retrofit2.Callback<ResponseSignUpDto> {
+                    override fun onResponse(
+                        call: Call<ResponseSignUpDto>,
+                        response: Response<ResponseSignUpDto>
+                    ) {
+                        if (response.isSuccessful) {
+                            response.body()?.message?.let {
+                                Toast.makeText(
+                                    this@SignUpActivity,
+                                    response.body()?.message ?: "회원가입 성공",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                            }
+                            val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+                            setResult(RESULT_OK, intent)
+                            finish()
+                        } else {
+                            Toast.makeText(this@SignUpActivity, "회원가입 실패", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<ResponseSignUpDto>, t: Throwable) {
+                        Toast.makeText(this@SignUpActivity, "서버 통신 실패", Toast.LENGTH_SHORT).show()
+                    }
+                })
+
+            } else {
                 Snackbar.make(
                     binding.root,
                     "회원가입이 실패했다.",
@@ -55,7 +93,6 @@ class SignUpActivity : AppCompatActivity() {
 
         }
     }
-
 
 
 }
