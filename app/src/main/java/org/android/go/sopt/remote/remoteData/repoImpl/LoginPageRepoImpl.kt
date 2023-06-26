bb626026efd5bcf19380f57a7f632f5417cfa468
gaeun <geun5744@gmail.com>
2023-06-26 22:38:11 +0900

    [Fix/#9] Repo interface 상속 추가

--- app/src/main/java/org/android/go/sopt/remote/remoteData/repoImpl/LoginPageRepoImpl.kt
+++ app/src/main/java/org/android/go/sopt/remote/remoteData/repoImpl/LoginPageRepoImpl.kt
@@ -2,22 +2,23 @@ package org.android.go.sopt.remote.remoteData.repoImpl
 
 import org.android.go.sopt.RequestSignUpDto
 import org.android.go.sopt.ResponseSignUpDto
+import org.android.go.sopt.remote.domain.LoginPageRepo
 import org.android.go.sopt.remote.remoteData.datasource.LoginPageDataSource
 import org.android.go.sopt.remote.remoteData.model.MyProfileDto
 import org.android.go.sopt.remote.remoteData.model.RequestLogInDto
 import org.android.go.sopt.remote.remoteData.model.ResponseLogInDto
 
-class LoginPageRepoImpl(private val loginPageDataSource: LoginPageDataSource) {
+class LoginPageRepoImpl(private val loginPageDataSource: LoginPageDataSource):LoginPageRepo {
 
-    suspend fun login(request: RequestLogInDto): ResponseLogInDto {
+    override suspend fun login(request: RequestLogInDto): ResponseLogInDto {
         return loginPageDataSource.login(request)
     }
 
-    suspend fun signUp(request: RequestSignUpDto): ResponseSignUpDto {
+    override suspend fun signUp(request: RequestSignUpDto): ResponseSignUpDto {
         return loginPageDataSource.signUp(request)
     }
 
-    suspend fun myProfile(userId: String): MyProfileDto {
+    override suspend fun myProfile(userId: String): MyProfileDto {
         return loginPageDataSource.myProfile(userId)
     }
 }
--- app/src/main/java/org/android/go/sopt/remote/remoteData/repoImpl/MainPageRepoImpl.kt
+++ app/src/main/java/org/android/go/sopt/remote/remoteData/repoImpl/MainPageRepoImpl.kt
@@ -1,12 +1,13 @@
 package org.android.go.sopt.remote.remoteData.repoImpl
 
+import org.android.go.sopt.remote.domain.MainPageRepo
 import org.android.go.sopt.remote.remoteData.datasource.MainPageDataSource
 import org.android.go.sopt.remote.remoteData.model.ResponseListUsersDto
 
 class MainPageRepoImpl(
     private val mainPagerDataSource: MainPageDataSource,
-) {
-    suspend fun getUserList(): ResponseListUsersDto {
+) : MainPageRepo {
+    override suspend fun getUserList(): ResponseListUsersDto {
         return mainPagerDataSource.getUserList()
     }
 }
