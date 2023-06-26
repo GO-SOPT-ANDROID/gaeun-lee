dafd061cbb98593d181523173f1089bc51f8cb34
gaeun <geun5744@gmail.com>
2023-06-26 22:00:25 +0900

    [Delete/#9] 반환 타입에서 Response 객체 제거

--- app/src/main/java/org/android/go/sopt/remote/domain/MainPageRepo.kt
+++ app/src/main/java/org/android/go/sopt/remote/domain/MainPageRepo.kt
@@ -1,9 +1,8 @@
 package org.android.go.sopt.remote.domain
 
 import org.android.go.sopt.remote.remoteData.model.ResponseListUsersDto
-import retrofit2.Response
 
 interface MainPageRepo {
 
-    suspend fun getUserList():Response<ResponseListUsersDto>
+    suspend fun getUserList(): ResponseListUsersDto
 }
