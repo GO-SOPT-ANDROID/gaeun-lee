74450a825a7f876c95c834ec4efbab4129e188dc
gaeun <geun5744@gmail.com>
2023-06-27 23:00:10 +0900

    [Refactor/#9] SharedPreference 전역에서 공유
    viewModel에서 사용할 수 있도록 SharedPrefernces를 Application 클래스로 옮김

--- app/src/main/java/org/android/go/sopt/present/menuFragment/MyProfileFragment.kt
+++ app/src/main/java/org/android/go/sopt/present/menuFragment/MyProfileFragment.kt
@@ -7,16 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import org.android.go.sopt.databinding.FragmentMyProfileBinding
 import org.android.go.sopt.present.loginPage.LoginActivity
-import org.android.go.sopt.present.loginPage.MySharedPreferences
-import org.android.go.sopt.present.viewModel.LoginViewModel
+import org.android.go.sopt.util.MySharedPreferences
 import org.android.go.sopt.remote.ServicePool
-import org.android.go.sopt.remote.remoteData.model.MyProfileDto
-import org.android.go.sopt.util.makeToastMessage
-import retrofit2.Call
-import retrofit2.Response
+import org.android.go.sopt.util.MyApplication
 
 class MyProfileFragment : Fragment() {
     private var _binding: FragmentMyProfileBinding? = null
@@ -24,12 +19,11 @@ class MyProfileFragment : Fragment() {
         get() = requireNotNull(_binding) { "앗 ! _binding이 null이다 !" }
 
     private val myProfileService = ServicePool.loginPageService
-    private val viewModel by viewModels<LoginViewModel>()
 
     override fun onCreateView( // 뷰를 만든다 << 이때 초기화하면 좋음
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View { // 이제 반환하는 View가 Null일 수 없기 때문에, ?를 지워주셔도 됩니다.
         _binding = FragmentMyProfileBinding.inflate(inflater, container, false)
         return binding.root
@@ -40,7 +34,6 @@ class MyProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         getProfile()
         clickLogOut()
-
     }
 
     override fun onDestroyView() {
@@ -49,14 +42,22 @@ class MyProfileFragment : Fragment() {
     }
 
     private fun getProfile() {
+        /*myProfileService.myProfile(
+            MySharedPreferences.getUserId(requireContext())
+        ).enqueue(object : retrofit2.Callback<MyProfileDto> {
+            override fun onResponse(call: Call<MyProfileDto>, response: Response<MyProfileDto>) {
+                if (response.isSuccessful) {
+                    binding.name.text = "이름 : ${response.body()?.data?.name}"
+                    binding.specialty.text = "특기 : ${response.body()?.data?.skill}"
+                } else {
+                    requireContext().makeToastMessage("서버 실패")
+                }
+            }
 
-        viewModel.myProfile(MySharedPreferences.getUserId(requireContext()))
-
-        viewModel.myProfile.observe(this){
-            binding.name.text = "이름 : ${it.data.name}"
-            binding.specialty.text = "특기 : ${it.data.skill}"
-        }
-
+            override fun onFailure(call: Call<MyProfileDto>, t: Throwable) {
+                requireContext().makeToastMessage("서버 실패")
+            }
+        })*/
     }
 
     private fun clickLogOut() {
@@ -64,14 +65,12 @@ class MyProfileFragment : Fragment() {
             val builder = AlertDialog.Builder(requireContext())
             builder.setTitle("Go SOPT").setMessage("로그아웃 하시겠나요?")
                 .setPositiveButton("네") { _, _ ->
-                    MySharedPreferences.clearUser(requireContext())
+                    MyApplication.mySharedPreferences.clearUser()
                     val intent = Intent(activity, LoginActivity::class.java)
                     startActivity(intent)
                     requireActivity().finish()
-
                 }.setNegativeButton("아니요", null).setCancelable(true)
             builder.show()
-
         }
     }
 }
---
+++ app/src/main/java/org/android/go/sopt/util/MySharedPreferences.kt
@@ -0,0 +1,56 @@
+package org.android.go.sopt.util
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class MySharedPreferences(context: Context) {
+    private val MY_ACCOUNT: String = "account"
+    private val prefs: SharedPreferences =
+        context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+
+    fun setUserId(input: String) {
+        val editor: SharedPreferences.Editor = prefs.edit()
+        editor.putString("MY_ID", input)
+        editor.commit()
+    }
+
+    fun getUserId(): String {
+        return prefs.getString("MY_ID", "").toString()
+    }
+
+    fun setUserPass(input: String) {
+        val editor: SharedPreferences.Editor = prefs.edit()
+        editor.putString("MY_PASS", input)
+        editor.commit()
+    }
+
+    fun getUserPass(): String {
+        return prefs.getString("MY_PASS", "").toString()
+    }
+
+    fun setUserName(input: String) {
+        val editor: SharedPreferences.Editor = prefs.edit()
+        editor.putString("MY_NAME", input)
+        editor.commit()
+    }
+
+    fun getUserName(): String {
+        return prefs.getString("MY_NAME", "").toString()
+    }
+
+    fun setUserSpec(input: String) {
+        val editor: SharedPreferences.Editor = prefs.edit()
+        editor.putString("MY_SPEC", input)
+        editor.commit()
+    }
+
+    fun getUserSpec(): String {
+        return prefs.getString("MY_SPEC", "").toString()
+    }
+
+    fun clearUser() {
+        val editor: SharedPreferences.Editor = prefs.edit()
+        editor.clear()
+        editor.commit()
+    }
+}
